a712a37d848ecb1b4e91ae78fc1bd6e57d2495d4
Tiago Sousa Rocha <tsrrocha@gmail.com>
2024-11-20 23:08:17 -0300

    Implementada a lista encadeada para armazenar os cartões cadastrados.

--- src/P8ProjetoAnaliseCircuito_AccessControlRFID.ino/P8ProjetoAnaliseCircuito_AccessControlRFID.ino.ino
+++ src/P8ProjetoAnaliseCircuito_AccessControlRFID.ino/P8ProjetoAnaliseCircuito_AccessControlRFID.ino.ino
@@ -3,7 +3,7 @@
  *       Disciplina: Análise de Circuitos
  *       Dupla: 
  *          Tiago Sousa Rocha
- *          Jonatan
+ *          Jonathan Pereira
  *       Descricao:
  *          Este circuito permite realizar o controle de acesso através do acionamento de uma saída mediante
  *       A liberação do acesso por meio da leitura de um cartao RFID que esteja cadastrado no sistema.
@@ -18,16 +18,23 @@
 /** -------------------------*/
 /**  Definicoes do projeto   */
 /** -------------------------*/
-// Maximo de Cartoes que podem ser cadastrados
-#define MAX_CARD 10
+#define MAX_CARD          10  // Maximo de Cartoes que podem ser cadastrados
+#define BASE_TIME_DELAY   200
+#define BASE_1S           (1000 / BASE_TIME_DELAY)
+#define T_1_SEG           (1 * BASE_1S)
+#define T_3_SEG           (3 * BASE_1S)
+#define T_5_SEG           (5 * BASE_1S)
+#define T_10_SEG          (10 * BASE_1S)
 
-#define LED 8             // Define o pino do LED
-#define ATUADOR 7         // Pino de acionamento do rele
-#define ADD_CARD 6        // Pino de cadastramento de cartao
-#define DEL_CARD 5        // Pino de remocao de cartao
-#define RFID_RST_PIN 9    // Define o pino de reset do leitor RFID
-#define RFID_SS_PIN 10    // Pino de seleção do leitor RFID
-MFRC522 mfrc522(RFID_SS_PIN, RFID_RST_PIN);
+#define TEMPO_LED         T_1_SEG
+#define TEMPO_SAIDA       T_5_SEG
+#define LED_OK            3   // Define o pino do LED
+#define LED_FAIL          2   // Define o pino do LED
+#define ATUADOR           7   // Pino de acionamento do rele
+#define ADD_CARD          6   // Pino de cadastramento de cartao
+#define DEL_CARD          5   // Pino de remocao de cartao
+#define RFID_RST_PIN      9   // Define o pino de reset do leitor RFID
+#define RFID_SS_PIN       10  // Pino de seleção do leitor RFID
 
 //  RST   -> Pin 9
 //  MISO  -> Pin 12
@@ -35,91 +42,225 @@ MFRC522 mfrc522(RFID_SS_PIN, RFID_RST_PIN);
 //  SCK   -> Pin 13
 //  SDA   -> Pin 10
 
+
+MFRC522 mfrc522(RFID_SS_PIN, RFID_RST_PIN);
+
+
+
 /** -------------------------*/
 /**  Estrutura de dados      */
 /** -------------------------*/
-typedef unsigned char BYTE;
-typedef unsigned long DWORD;
+typedef unsigned char BYTE;     // 1 byte
+typedef unsigned long DWORD;    // 4 bytes
+
+// Lista Encadeada
+struct TNode {
+  DWORD cardnumber;
+  TNode* next;  
+} ;
+
+typedef struct {
+  byte qty;
+  TNode *first;
+} TList;
 
 // Estrutura de dados da aplicacao
 typedef struct {
   struct {
     unsigned add: 1;    // Estado de ADICIONAR
     unsigned del: 1;
-    unsigned read: 1;
+    unsigned activeOutput: 1;
+    unsigned ledOK: 1;
+    unsigned ledFail: 1;
   } state;
   DWORD card[MAX_CARD];
+  TList cards;
+  BYTE qtyCard;
 }TDataApplication;
+
 
 /** -------------------------*/
 /**  Variaveis globais       */
 /** -------------------------*/
 TDataApplication app;
-char msg[40];
+char msg[60];
+byte showList = 0;                // flag para sinalizar que deve imprimir a lista
+byte cntTimerActiveOutput = 0;    // Contador para temporizar o desacionamento da saida
+byte cntTimerLEDOK = 0;           // Contador para temporizar o desacionamento do LED de OK
+byte cntTimerLEDFail = 0;         // Contador para temporizar o desacionamento do LED de Fail
+
 
 
 /** -------------------------*/
 /**  Prototipos de funcao    */
 /** -------------------------*/
 DWORD verificaNovoCartao();
+byte verificaAcessoLiberado(DWORD card);
+
+byte cardExists(TList* lst, DWORD card);
+byte addCardIntoList(TList* lst, DWORD card);
+byte delCardIntoList(TList * lst, DWORD card);
+
+
+
+
+
 
 void setup() {
   Wire.begin();       //INICIALIZA A BIBLIOTECA WIRE
   SPI.begin();        //INICIALIZA O BARRAMENTO SPI
   mfrc522.PCD_Init(); //INICIALIZA MFRC522
-  
-  app.state.add = 0;
-  app.state.del = 0;
-  app.state.read = 0;
-  app.card[0] = 0;
 
-  // put your setup code here, to run once:
+  // 
+  memset(&app, 0, sizeof(TDataApplication));
+
+  // 
   Serial.begin(9600);
+  
   // Configura os pinos que serao saidas (OUTPUT)
-  pinMode(LED, OUTPUT);
+  pinMode(LED_OK, OUTPUT);
+  pinMode(LED_FAIL, OUTPUT);
   pinMode(ATUADOR, OUTPUT);
+  
   // Configura os pinos que serão entradas (INPUT)
-  pinMode(ADD_CARD, INPUT);
-  pinMode(DEL_CARD, INPUT);
+  pinMode(ADD_CARD, INPUT_PULLUP);
+  pinMode(DEL_CARD, INPUT_PULLUP);
 
   //
-  digitalWrite(LED, HIGH);
+  digitalWrite(LED_OK, LOW);
+  digitalWrite(LED_FAIL, LOW);
+  digitalWrite(ATUADOR, LOW);
 
   Serial.write("==============================\tINICIANDO\t================\n");
-  /*Serial.write("SizeOf DWORD: ");
-  sprintf(msg, "SizeOf: %d\n", sizeof(DWORD));
+  sprintf(msg, "Total de cartões cadastrados: %d\n", app.qtyCard);
   Serial.write(msg);
-  //*/
+  Serial.write("==============================\t         \t================\n");
 }
 
-void loop() {
-    DWORD cardRead = 0;
-  
-    // Le a entrada de cadastramento
-    app.state.add =  (digitalRead(ADD_CARD)==1)? 1 : 0;
-    // Le a entrada de Remocao de cartoes
-    app.state.del =  (digitalRead(DEL_CARD)==1)? 1 : 0;
-    // Define o estado do READ
-    if ((app.state.add == 1) || (app.state.del == 1)) {
-      //Serial.write ("DEBUG: Eh para fazer cadastro e remocao de cartao\n");
-      // É para fazer o cadastramento ou remocao do cartao
-      app.state.read = 1;
-    } else {
-      //Serial.write ("DEBUG: Eh para fazer a leitura de cartao\n");
-      // É para fazer a leitura do cartao e liberar o acesso
-      app.state.read = 0;
-    }
 
-    
-    cardRead = verificaNovoCartao();
-    if (cardRead > 0) {
-      // Verifica se é para Adicionar o cartao lido?
-      // Verifica se é para Remover o cartao lido?
-      // Verifica se é para checar o acesso para o cartao lido?
+
+void loop() {
+  DWORD cardRead = 0;
+
+  // Lê o estado das entradas para Operação de Cadastramento ou Remoção de cartão
+  app.state.add =  (digitalRead(ADD_CARD)==0)? 1 : 0;
+  app.state.del =  (digitalRead(DEL_CARD)==0)? 1 : 0;
+
+  // Lê um cartão RFID
+  cardRead = verificaNovoCartao();
+  if (cardRead > 0) {
+    // Se operação for para Cadastrar
+    if (app.state.add == 1) {
+      if (app.cards.qty >= MAX_CARD) {
+        sprintf(msg, "DEBUG: Memória cheia! Cartão não foi cadastrado. (Qty: %d)\n", app.cards.qty);
+        Serial.write(msg);
+      } else {
+        showList = 1;   // Ativa a impressão da lista
+        byte res = addCardIntoList(&app.cards, cardRead);
+        if (res == 1) {
+          sprintf(msg, "DEBUG: Cartão %lu cadastrado com sucesso! (Qty: %d)\n", cardRead, app.cards.qty);
+          Serial.write(msg); 
+          app.state.ledOK = 1;
+          digitalWrite(LED_OK, HIGH);  
+        } else {
+          if (res == 2) {
+            sprintf(msg, "DEBUG: Cartão %lu já existe! (Qty: %d)\n", cardRead, app.cards.qty);
+          } else {
+            sprintf(msg, "ERROR: Falha ao cadastrar o cartão: %lu! (Qty: %d)\n", cardRead, app.cards.qty);
+            app.state.ledFail = 1;
+            digitalWrite(LED_FAIL, HIGH); 
+          }
+          Serial.write(msg);  
+        }
+      }
+    } else if (app.state.del == 1) {
+      // Se operação for de Remover o cartao
+      if (app.cards.qty <= 0) {
+        sprintf(msg, "DEBUG: Lista vazia! (Qty: %d)\n", app.cards.qty);
+        Serial.write(msg);  
+      } else {
+        showList = 1;   // Ativa a impressão da lista
+        if (delCardIntoList(&app.cards, cardRead) == 1) {
+          sprintf(msg, "DEBUG: Cartão %lu removido com sucesso! (Qty: %d)\n", cardRead, app.cards.qty);
+          Serial.write(msg);  
+          app.state.ledOK = 1;
+          digitalWrite(LED_OK, HIGH);
+        } else {
+          sprintf(msg, "ERROR: Falha ao remover o cartão: %lu! (Qty: %d)\n", cardRead, app.cards.qty);
+          Serial.write(msg); 
+          app.state.ledFail = 1;
+          digitalWrite(LED_FAIL, HIGH); 
+        }
+      }
+    } else {
+      // Se operação é para liberação de acesso
+      if (cardExists(&app.cards, cardRead) == 1) {
+        showList = 1;   // Ativa a impressão da lista
+        app.state.activeOutput = 1;
+        app.state.ledOK = 1;
+        digitalWrite(ATUADOR, HIGH);
+        digitalWrite(LED_OK, HIGH);
+        sprintf(msg, "DEBUG: Acesso Liberado o cartão: %lu! (Qty: %d)\n", cardRead, app.cards.qty);
+        Serial.write(msg);
+      } else {
+        app.state.ledFail = 1;
+        digitalWrite(LED_FAIL, HIGH);
+        sprintf(msg, "DEBUG: Acesso Negado!\n");
+        Serial.write(msg);
+      }
     }
-    
+  }
+
+  // LED OK
+  if (app.state.ledOK == 1) {
+    cntTimerLEDOK++;
+    if( cntTimerLEDOK >= TEMPO_LED) {
+      cntTimerLEDOK = 0;
+      app.state.ledOK = 0;
+      digitalWrite(LED_OK, LOW);
+      Serial.write("DEBUG: DESATIVA LED_OK\n");
+    }
+  }
+
+  // LED FAIL
+  if (app.state.ledFail == 1) {
+    cntTimerLEDFail++;
+    if( cntTimerLEDFail >= TEMPO_LED) {
+      cntTimerLEDFail = 0;
+      app.state.ledFail = 0;
+      digitalWrite(LED_FAIL, LOW);
+      Serial.write("DEBUG: DESATIVA LED_FAIL\n");
+    }
+  }
+
+  if (app.state.activeOutput == 1) {
+    cntTimerActiveOutput ++;
+    if(cntTimerActiveOutput >= TEMPO_SAIDA) {
+      cntTimerActiveOutput = 0;
+      app.state.activeOutput = 0;
+      digitalWrite(ATUADOR, LOW);
+      Serial.write("DEBUG: DESATIVA SAIDA\n");
+    }
+  }
+
+  if (showList == 1) {
+    showList = 0;
+    sprintf(msg, "\n Imprime lista de cartões (Qty: %d):\n", app.cards.qty);
+    Serial.write(msg);
+    TNode* No = app.cards.first;
+    byte idx = 0;
+    do {
+      if (No != NULL) {
+        sprintf(msg, "[%0u] Card: %lu\n", idx, No->cardnumber);
+        Serial.write(msg);
+      }
+      idx++;
+      No = No->next;
+    }while(No);
+  }
   
-    delay(100);
+  
+  delay(BASE_TIME_DELAY);
 }
 
 
@@ -134,27 +275,115 @@ DWORD verificaNovoCartao()
       return 0; 
       
     // Leu um novo cartao RFID
-    String strID = ""; 
-    //Serial.write("DEBUG: [");
     for (byte i = 0; i < 4; i++) {
       cardnumber = mfrc522.uid.uidByte[i] | (cardnumber << 8);
-      strID += (mfrc522.uid.uidByte[i] < 0x10 ? "0" : "") + String(mfrc522.uid.uidByte[i], HEX) + (i!=3 ? ":" : "");
-      //sprintf(msg, "(%d) ", mfrc522.uid.uidByte[i]);      
-      //Serial.write(msg);
     }    
-    //Serial.write("]\n");
-
-    sprintf(msg, "DEBUG: Cartao lido: %lu \n", cardnumber);      
-    Serial.write(msg);
-    
-    //strID.toUpperCase();
-    //char msg[30];
-    //sprintf(msg, "DEBUG: Novo cartao lido: %s\n", strID);
+    //sprintf(msg, "DEBUG: Cartao lido: %lu \n", cardnumber);      
     //Serial.write(msg);
-
-
+ 
     mfrc522.PICC_HaltA(); //PARADA DA LEITURA DO CARTÃO
     mfrc522.PCD_StopCrypto1(); //PARADA DA CRIPTOGRAFIA NO PCD
 
     return cardnumber;
-}
+};
+
+byte verificaAcessoLiberado(DWORD card)
+{
+    for (byte i = 0; i < app.qtyCard; i++) {
+      if (app.card[i] == card) {
+        return 1;
+      }
+    }
+    return 0;
+};
+
+byte cardExists(TList* lst, DWORD card) {
+  TNode *no;
+  if (lst->first == NULL) {
+    lst->qty = 0;
+    return 0;
+  } else {
+    no = lst->first;    
+    while(no != NULL) {
+      if (no->cardnumber == card){
+        return 1;
+      }
+      no = no->next;
+    }
+    if (no->cardnumber == card){
+      return 1;
+    }
+    return 0;
+  }
+};
+
+byte addCardIntoList(TList * lst, DWORD card) {
+  TNode *no;
+  if (cardExists(lst, card) == 0) {
+    // Cartão não existe.
+    if (lst->qty >= MAX_CARD) {
+      return 3;
+    } else {
+      if (lst->first == NULL) {
+        // Lista vazia
+        lst->first = (TNode *) malloc (sizeof(TNode));
+        lst->first->cardnumber = card;
+        lst->first->next = NULL;
+        lst->qty = 1;
+        return 1;
+      } else {
+        no = lst->first;
+        while(no->next != NULL) {
+          no = no->next;
+        }
+        no->next = (TNode *) malloc (sizeof(TNode));
+        no->next->next = NULL;
+        no->next->cardnumber = card;
+        lst->qty ++;
+        return 1;
+      }
+    }
+  } else {
+    // Cartão já cadastrado
+    return 2;
+  }
+  return 0;
+};
+
+byte delCardIntoList(TList * lst, DWORD card) {
+  TNode *noAtual;
+  TNode *noAnterior;
+  if (cardExists(lst, card) != 0) {
+    // Cartão não existe.
+    if (lst->qty <= 0) {
+      return 0;
+    } else {
+      noAnterior = NULL;
+      noAtual = lst->first;
+      do {
+        if (noAtual != NULL) {
+          if (noAtual->cardnumber == card) {
+            // Achou o cartao a remover
+            if (noAnterior == NULL) {
+              // é o primeiro da fila
+              lst->first = noAtual->next;
+              lst->qty--;
+              free(noAtual);
+              return 1;
+            } else {
+              // está no meio ou fim da fila
+              noAnterior->next = noAtual->next;
+              lst->qty--;
+              free(noAtual);
+              return 1;
+            }
+          } else {
+            noAnterior = noAtual;
+            noAtual = noAtual->next;
+          }
+        }
+      } while (noAtual);
+    }
+  }
+  return 0;
+};
